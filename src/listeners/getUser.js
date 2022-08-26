9d060b954df9cc4bc00395cbcfb3d176caaee44a
Samir M <67711948+samirm00@users.noreply.github.com>
2022-08-26 17:30:00 +0200

    Update getUser.js

--- src/listeners/getUser.js
+++ src/listeners/getUser.js
@@ -3,12 +3,12 @@ import getUserHandler from "../handlers/getUser.js"
 
 // DOM ELEMENT 
 const userButton  = document.getElementById("get-user");
-const UserIdInput = document.getElementById("userId");
+const userIdInput = document.getElementById("userId");
 
 userButton.addEventListener("click", function(e)
 {
     e.preventDefault();
 
-    const userId = UserIdInput.value;
+    const userId = userIdInput.value;
     getUserHandler(userId);
 });
